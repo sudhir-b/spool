98b08fe4400737dc456c9c416ea83251a27e8c71
sudhir-b <sud.balaji@gmail.com>
2022-02-16 21:04:52 +0000

    update program ID

--- programs/spool/src/lib.rs
+++ programs/spool/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("6jh18NDLTASsVvoZYSGwY3n9nDWmY8bVLAQhoYphuXTs");
 
 #[program]
 pub mod spool {
